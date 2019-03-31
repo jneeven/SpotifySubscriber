e9f5d661aed78a5564c7807b914800a5836bbb26
Jelmer <jelmer.neeven@gmail.com>
2019-03-31 21:16:50 +0200

    Changed safe_print to properly print objects with their __repr__ function

--- src/SpotifySubscriber.py
+++ src/SpotifySubscriber.py
@@ -15,7 +15,10 @@ def safe_print(*args):
     try:
         print(*args)
     except UnicodeEncodeError:
-        string = " ".join(args)
+        string = ""
+        for arg in args[:-1]:
+            string += str(arg) + " "
+        string += str(args[-1])
         print(string.encode('utf-8'))
 
 
